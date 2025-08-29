a16178d5f989d8b7464ea3a91278eb72349e29b0
Papellero <ACT-STUDENT@LAB3-PC28>
2025-08-29 14:40:56 +0800

    Hi

---
+++ Cuyos_COConstructorUserInput/Cuyos_COConstructorUserInput/Footwear.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuyos_COConstructorUserInput
+{
+    internal class Footwear
+    {
+        // Declaring Fields
+        public string Brand;
+        public string Color;
+        public double Size;
+
+
+        public Footwear(string aBrand, string aColor, double aSize) // Parameterize Constructors
+        {
+            Brand = aBrand;
+            Color = aColor;
+            Size = aSize;
+        }
+
+        // Method to Display
+        public void DisplayFootwear()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("******** Here are the Details of the Shoes you inputed *********");
+            Console.WriteLine("\nEnter the Brand of your shoes: " + Brand);
+            Console.WriteLine("Enter the Color of your shoes: " + Color);
+            Console.WriteLine("Enter the Size of your shoes : " + Size);
+        }
+    }
+}
---
+++ Cuyos_COConstructorUserInput/Cuyos_COConstructorUserInput/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuyos_COConstructorUserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Main:
+            // Louis Aian Cuyos
+            // IT306
+            // EXER_03: User Input with Constructor
+
+
+
+            // User input
+            Console.ForegroundColor = ConsoleColor.Yellow;// Color
+
+            Console.WriteLine("\t\t\t\tEnter the Brand,Color,Size of your Favorite Shoes ");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;// Color
+            Console.Write("\nEnter the Brand of your shoes : ");
+            string aBrand = Console.ReadLine();
+
+            Console.Write("Enter the Color of you shoes  : ");
+            string aColor = Console.ReadLine();
+
+            Console.Write("Enter the Size of your shoes  : ");
+            double aSize = Convert.ToDouble(Console.ReadLine());
+
+
+            // Instantiation of an object
+            Footwear myFootwear = new Footwear(aBrand, aColor, aSize);
+            myFootwear.DisplayFootwear();
+
+
+            // If you want to input again
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\nEnter if you want more (Y/N)");
+            char i = Convert.ToChar(Console.ReadLine());
+
+            if (i == 'Y')
+            {
+                goto Main;
+            }
+            else
+            {
+                return;
+            }
+
+        }
+    }
+}
